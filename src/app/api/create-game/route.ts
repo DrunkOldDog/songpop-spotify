8dc1c50ac38bff3cb693a379e570d8c35f250e09
Juan Reyes <juanisoul@gmail.com>
2023-12-14 23:20:06 -0400

    Add workaround with create-game api

---
+++ src/app/api/create-game/route.ts
@@ -0,0 +1,6 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  return NextResponse.json(body, { status: 200 });
+}
--- src/app/components/CreateGame/CreateGame.tsx
+++ src/app/components/CreateGame/CreateGame.tsx
@@ -3,12 +3,21 @@
 import { Button, Input } from "@chakra-ui/react";
 import { FormEventHandler, useRef } from "react";
 
+import type { CreateGameBody } from "@/app/types";
+import axios from "axios";
+import { SERVER } from "@/app/common/server";
+
 export const CreateGame = () => {
   const playlistInputRef = useRef<HTMLInputElement>(null);
 
   const onSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
-    console.log(playlistInputRef.current?.value);
+    const body: CreateGameBody = {
+      playlistId: playlistInputRef.current?.value ?? "",
+    };
+
+    const data = await axios.post(SERVER.CREATE_GAME, body);
+    console.log({ data });
   };
 
   return (
