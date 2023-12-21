f99544841093e095206c2d07f3649706b76ce337
Juan Reyes <juanisoul@gmail.com>
2023-12-20 21:46:50 -0400

    Solve multiple clients created in redis config

--- src/app/lib/redis/db.ts
+++ src/app/lib/redis/db.ts
@@ -1,12 +1,27 @@
 import { createClient } from "redis";
 
-const client = createClient({
-  password: process.env.REDIS_PASSWORD,
-  socket: {
-    host: process.env.REDIS_HOST,
-    port: Number(process.env.REDIS_PORT),
-  },
-});
+// Singleton approach based on:
+// https://www.prisma.io/docs/orm/more/help-and-troubleshooting/help-articles/nextjs-prisma-client-dev-practices
+
+declare global {
+  var redisClient: undefined | ReturnType<typeof redisClientSingleton>;
+}
+
+const redisClientSingleton = () => {
+  return createClient({
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+      host: process.env.REDIS_HOST,
+      port: Number(process.env.REDIS_PORT),
+    },
+  });
+};
+
+const client = globalThis.redisClient ?? redisClientSingleton();
+
+if (process.env.NODE_ENV !== "production") {
+  globalThis.redisClient = client;
+}
 
 client.on("error", (err) => console.error(err));
 
