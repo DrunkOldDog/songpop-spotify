b1735a40fbd3a1761d19c0627ecb5da4d6d65e91
Juan Reyes <juanisoul@gmail.com>
2023-12-25 20:24:50 -0400

    Add flag condition to prevent multiple getSession calls

--- src/context/AuthProvider/AuthProvider.tsx
+++ src/context/AuthProvider/AuthProvider.tsx
@@ -1,17 +1,25 @@
 "use client";
 
 import { getSession, signIn, useSession } from "next-auth/react";
-import { useEffect, type ReactNode } from "react";
+import { useEffect, type ReactNode, useRef } from "react";
 
 interface AuthProviderProps {
   children?: ReactNode;
 }
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const flag = useRef(false);
   const { data: session } = useSession();
 
   useEffect(() => {
     (async () => {
+      // added to prevent multiple calls on fast refresh and react strict
+      if (flag.current) {
+        return;
+      }
+
+      flag.current = true;
+
       // have to run each on app initialization to call jwt callback and validate refreshToken
       await getSession();
     })();
