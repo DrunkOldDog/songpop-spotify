d68b27c6eb6bd5dec512b36aed0caf4841967198
Juan Reyes <juanisoul@gmail.com>
2023-12-27 21:34:38 -0400

    Update game.types

--- src/common/types/game.types.ts
+++ src/common/types/game.types.ts
@@ -1,7 +1,18 @@
-import type { GameStatus } from ".";
+import type { GameStatus, Playlist, TrackItem } from ".";
+
+export type GameTrack = TrackItem & {
+  isCurrent: boolean;
+};
+
+type PlayerScore = {
+  nickname: string;
+  score: number;
+};
 
 export type Game = {
-  playlistId: string;
   createdBy: string;
   gameStatus: GameStatus;
+  playlist: Playlist;
+  tracks: GameTrack[][];
+  playersScore: PlayerScore[];
 };
