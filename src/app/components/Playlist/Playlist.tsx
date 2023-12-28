f084917d2a11370b6a29c66cad1c02f0df52db5c
Juan Reyes <juanisoul@gmail.com>
2023-12-27 20:58:02 -0400

    Add Playlist client component

---
+++ src/app/components/Playlist/Playlist.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Box, Heading, Image, Text } from "@chakra-ui/react";
+
+import type { Playlist as PlaylistType } from "@/common/types";
+
+interface PlaylistProps {
+  playlist: PlaylistType;
+}
+
+export const Playlist = ({ playlist }: PlaylistProps) => (
+  <Box display={"flex"} flexDir={"column"} alignItems={"center"}>
+    <Image
+      alt={playlist.name}
+      src={playlist.images[0].url}
+      height={[40, 48, 56, 56]}
+      mb={4}
+    />
+
+    <Box mb={8} maxW={360} textAlign="center">
+      <Heading as="h2" size={"lg"}>
+        {playlist.name}
+      </Heading>
+      <Text>{playlist.description}</Text>
+    </Box>
+  </Box>
+);
