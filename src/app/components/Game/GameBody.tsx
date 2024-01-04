b08cdaeee909e90f876fda39e6e944089fa3e1af
Juan Reyes <juanisoul@gmail.com>
2024-01-03 22:53:36 -0400

    Move score handler to GameBody

--- src/app/components/Game/GameBody.tsx
+++ src/app/components/Game/GameBody.tsx
@@ -1,20 +1,28 @@
 import { Score } from "./Score";
 import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 import { GameOptions, type GameOptionsProps } from ".";
-import { isGameFinished, isGameNotStarted } from "@/common/helpers";
+import {
+  getSongScore,
+  isGameFinished,
+  isGameNotStarted,
+} from "@/common/helpers";
+import { useState } from "react";
 
-interface GameBodyProps extends GameOptionsProps {
-  score: number;
-  streak: number;
+interface GameBodyProps extends Omit<GameOptionsProps, "onSelect"> {
+  startTime: Date;
   currentRound: number;
+  onTrackSelect: (trackId: string) => void;
 }
 
 export const GameBody = ({
-  score,
-  streak,
+  startTime,
   currentRound,
+  onTrackSelect,
   ...gameOptionProps
 }: GameBodyProps) => {
+  const [score, setScore] = useState(0);
+  const [streak, setStreak] = useState(0);
+
   if (isGameNotStarted(currentRound)) {
     return <Heading size="md">Go and start the game dude!</Heading>;
   }
@@ -23,6 +31,21 @@ export const GameBody = ({
     return <Score score={score} />;
   }
 
+  const onSelect = (trackId: string, isCorrect: boolean) => {
+    onTrackSelect(trackId);
+
+    if (isCorrect) {
+      const endTime = new Date();
+      const totalTime = (endTime.getTime() - startTime.getTime()) / 1000;
+
+      const newSongScore = Math.round(getSongScore(totalTime, streak));
+      setScore((prevScore) => prevScore + newSongScore);
+      setStreak((prevStreak) => prevStreak + 1);
+    } else {
+      setStreak(0);
+    }
+  };
+
   return (
     <Box w={"100%"}>
       <Flex justifyContent={"space-between"}>
@@ -30,7 +53,7 @@ export const GameBody = ({
         {streak > 0 && <Text fontWeight={"bold"}>{streak} 🔥</Text>}
       </Flex>
 
-      <GameOptions {...gameOptionProps} />
+      <GameOptions {...gameOptionProps} onSelect={onSelect} />
     </Box>
   );
 };
