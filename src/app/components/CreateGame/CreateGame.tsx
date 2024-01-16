f7fe122ad41aff1650ea4b4de88575529b93a581
Juan Reyes <juanisoul@gmail.com>
2024-01-15 23:05:49 -0400

    Add error handling on createGame

--- src/app/components/CreateGame/CreateGame.tsx
+++ src/app/components/CreateGame/CreateGame.tsx
@@ -1,12 +1,15 @@
 "use client";
 
-import { Button, Input } from "@chakra-ui/react";
+import { Button, Input, useToast } from "@chakra-ui/react";
 import { FormEventHandler, useRef, useState } from "react";
 import { createGame } from "@/app/actions/createGame";
 import { getSession } from "next-auth/react";
 import { getPlaylistIdFromInput } from "@/common/helpers";
 
+import type { AxiosError } from "axios";
+
 export const CreateGame = () => {
+  const toast = useToast();
   const [loading, setLoading] = useState(false);
   const playlistInputRef = useRef<HTMLInputElement>(null);
 
@@ -16,10 +19,23 @@ export const CreateGame = () => {
     setLoading(true);
     const { user } = (await getSession()) || {};
 
-    await createGame({
-      playlistId: getPlaylistIdFromInput(playlistInputRef.current?.value ?? ""),
-      userId: user!.userId,
-    });
+    try {
+      await createGame({
+        playlistId: getPlaylistIdFromInput(
+          playlistInputRef.current?.value ?? ""
+        ),
+        userId: user!.userId,
+      });
+    } catch (err) {
+      const error = err as AxiosError;
+      toast({
+        title: "Something went wrong",
+        description: error.message,
+        status: "error",
+        isClosable: true,
+      });
+    }
+
     setLoading(false);
   };
 
